9d215b931c4ee9de4844ad5f4f3a03d449f0a76f
RohitKumarSingh <rohity294@gmail.com>
2022-10-13 05:04:19 -0400

    initial commit

---
+++ src/app/autocomplete-filter-example.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
+/**
+ * @title Filter autocomplete
+ */
+@Component({
+  selector: 'autocomplete-filter-example',
+  templateUrl: 'autocomplete-filter-example.html',
+  styleUrls: ['autocomplete-filter-example.css'],
+})
+export class AutocompleteFilterExample implements OnInit {
+  myControl = new FormControl('');
+  options: string[] = ['Austria', 'Australia', 'India','Indonesia','China','Cameroon','Denmark','Egypt','Euthopia','England','France','Ghana','Germany','Honduras'];
+  filteredOptions: Observable<string[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+}
+
+
+/**  Copyright 2022 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
